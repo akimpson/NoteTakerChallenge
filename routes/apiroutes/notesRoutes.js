c3dc501ee9a4fdc6b2340e3ca7b8cf16ef3b05ca
Aisha Kimpson <akimp313@gmail.com>
2022-06-18 14:48:38 -0400

    updated the stylesheet folders

--- routes/apiroutes/notesRoutes.js
+++ routes/apiroutes/notesRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { createNote } = require("../../lib/notes.js");
+const { createNote, noteDeleteNote } = require("../../lib/notes.js");
 const { note } = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
@@ -7,19 +7,25 @@ router.get("/notes", (req, res) => {
   res.json(results);
 });
 
-router.get("/notes/:id", (req, res) => {
-  const result = findById(req.params.id, note);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
+//router.get("/notes/:id", (req, res) => {
+//  const result = findById(req.params.id, note);
+//  if (result) {
+//    res.json(result);
+//  } else {
+//    res.send(404);
+//  }
+// });
 
 router.post("/notes", (req, res) => {
-  // set id based on what thenext index of the array will be
+  // set id based on what the next index of the array will be
   req.body.id = note.length.toString();
   const notes = createNote(req.body, note);
+  res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  noteDeleteNote(notes, req.params.id);
+  const filteredNotes = notes.filter((note) => note.title !== title);
   res.json(notes);
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-//const port = process.env.port || 3000;
+//const port = process.env.port || 3005;
 const app = express();
 const apiroutes = require("./routes/apiroutes");
 const htmlRoutes = require("./routes/htmlRoutes");
@@ -12,6 +12,6 @@ app.use(express.static("public"));
 app.use("/api", apiroutes);
 app.use("/", htmlRoutes);
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+app.listen(process.env.PORT || 3005, "0.0.0.0", () => {
   console.log("Server is running.");
 });
